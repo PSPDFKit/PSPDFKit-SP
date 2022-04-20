ba06ce4cc1bffeadc32fbc0f0017faa518390d4b
Douglas Hill <hello@douglashill.co>
2022-04-20 16:29:20 +0100

    Try using URLs with distinct last path components to see if it works around the issue where Xcode 13 fails the download when opening the project for the first time

--- Package.swift
+++ Package.swift
@@ -12,11 +12,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PSPDFKit",
-            url: "https://customers.pspdfkit.com/pspdfkit/xcframework/11.3.1.zip",
+            url: "https://customers.pspdfkit.com/pspdfkit-xcframework-11.3.1.zip",
             checksum: "8b72c61db0497111cb5e94af2269c8e8a5a7b5935f5600d87264e1df266f0144"),
         .binaryTarget(
             name: "PSPDFKitUI",
-            url: "https://customers.pspdfkit.com/pspdfkitui/xcframework/11.3.1.zip",
+            url: "https://customers.pspdfkit.com/pspdfkitui-xcframework-11.3.1.zip",
             checksum: "d2ba1b1a01ac5a4cb36610200f8e13e671d8608608a6dcde46721e9b0ab04ab1"),
     ]
 )
