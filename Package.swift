0089f7f19ff673584bc4ca7ec9a163d4c5c51c87
Douglas Hill <hello@douglashill.co>
2023-03-09 12:49:42 +0100

    Update for PSPDFKit 12.1.2 for iOS

--- Package.swift
+++ Package.swift
@@ -16,11 +16,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PSPDFKit",
-            url: "https://customers.pspdfkit.com/pspdfkit-xcframework-12.1.1.zip",
-            checksum: "bc2bc22e3dd238180149458ab69d7c542140d912078ce9aef0735b49a1346c1f"),
+            url: "https://customers.pspdfkit.com/pspdfkit-xcframework-12.1.2.zip",
+            checksum: "204406ce4f5fc154e8dadf26dbf1561efa8eda2c254c06c16deb03718d216b98"),
         .binaryTarget(
             name: "PSPDFKitUI",
-            url: "https://customers.pspdfkit.com/pspdfkitui-xcframework-12.1.1.zip",
-            checksum: "605ed21386c5601d699ad083346445962079ad7e7ad6d6f7137e251c66803126"),
+            url: "https://customers.pspdfkit.com/pspdfkitui-xcframework-12.1.2.zip",
+            checksum: "cf7c352d51f525605563497263099afa08952f7344c744fa7788961f1d484fca"),
     ]
 )
