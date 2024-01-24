08711c7dbe01829ec7165b7a76835117bed104f3
Stefan Kieleithner <stefan.kieleithner@gmail.com>
2024-01-24 08:39:31 +0100

    Add visionOS support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let package = Package(
     name: "PSPDFKit",
     platforms: [
         .iOS(.v15),
-        .macCatalyst(.v15)
+        .macCatalyst(.v15),
+        .visionOS(.v1)
     ],
     products: [
         .library(
