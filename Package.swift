39c61f0746177e6f7795d6f9b62fe0c39a1d7de2
Stefan Kieleithner <stefan.kieleithner@gmail.com>
2024-09-16 11:06:47 +0200

    Update for PSPDFKit 14.0.0 for iOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -17,11 +17,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PSPDFKit",
-            url: "https://customers.pspdfkit.com/pspdfkit-xcframework-13.9.1.zip",
-            checksum: "e2a7ef3934058e89007b4336bc74da7f653f63b5cbf98b19a1017b02c3551172"),
+            url: "https://customers.pspdfkit.com/pspdfkit-xcframework-14.0.0.zip",
+            checksum: "61876f67c24c41209c514450fede281f175942bf43ed8a901070692cdb7f1f0d"),
         .binaryTarget(
             name: "PSPDFKitUI",
-            url: "https://customers.pspdfkit.com/pspdfkitui-xcframework-13.9.1.zip",
-            checksum: "068db21fa9c4730aa3c8b970422d0d15017801fd06b340c12bcf0a6f47595b85"),
+            url: "https://customers.pspdfkit.com/pspdfkitui-xcframework-14.0.0.zip",
+            checksum: "bd44f1ee76f3e780c1639462b2d4252ce4fd037cdf6db18c7c65e5d0d7b97a7b"),
     ]
 )
