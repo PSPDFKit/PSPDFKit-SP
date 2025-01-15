63d63a932acca943551f1774e04c14ab4777c974
Nishant Desai <nishdesai95@gmail.com>
2025-01-15 20:11:28 +0530

    Update for Nutrient iOS SDK 14.4.0

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PSPDFKit",
-            url: "https://customers.pspdfkit.com/pspdfkit-xcframework-14.3.0.zip",
-            checksum: "4fd303636378465726a22eba44f9a7e6d4962c3558a3b2980333e8b5a25c5bfe"),
+            url: "https://customers.pspdfkit.com/pspdfkit-xcframework-14.4.0.zip",
+            checksum: "f14b7ad8ae2ecd251407c286a1508642346fbcaea3e9f1620566336ee6f8a863"),
         .binaryTarget(
             name: "PSPDFKitUI",
-            url: "https://customers.pspdfkit.com/pspdfkitui-xcframework-14.3.0.zip",
-            checksum: "8c0bdf5f8f5401371635ff41bfcf3f5681551a1734f8fc314af9a308808fa4dd"),
+            url: "https://customers.pspdfkit.com/pspdfkitui-xcframework-14.4.0.zip",
+            checksum: "3ceadc67e03f16a975fd1f440650620b06c8d51cdab4278f1887dd26c6002821"),
     ]
 )
