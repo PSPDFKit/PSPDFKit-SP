7422bf221c71618ebd297806fc02ab27d9f1b4b7
Douglas Hill <hello@douglashill.co>
2026-01-14 22:42:23 +0000

    Update for Nutrient iOS SDK 26.4.0

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PSPDFKit",
-            url: "https://customers.pspdfkit.com/pspdfkit-xcframework-26.3.0.zip",
-            checksum: "fb1c3e06e407e0774a63444decf186610641a469a97f5a2003cec053f56b770c"),
+            url: "https://customers.pspdfkit.com/pspdfkit-xcframework-26.4.0.zip",
+            checksum: "44524b98f997c74edba441fa9680f0d61dae81ad7fc57ba814bee575b35838e0"),
         .binaryTarget(
             name: "PSPDFKitUI",
-            url: "https://customers.pspdfkit.com/pspdfkitui-xcframework-26.3.0.zip",
-            checksum: "d259d502801c8a6075e982169d7e598852e24e9725d7273ac091ced8463ea9bf"),
+            url: "https://customers.pspdfkit.com/pspdfkitui-xcframework-26.4.0.zip",
+            checksum: "d2b223cfb9abece3711e4a456d957a5cdc9215dce4b89e6817e379c04f31c00c"),
     ]
 )
